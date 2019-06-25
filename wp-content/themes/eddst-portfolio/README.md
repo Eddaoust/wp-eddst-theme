afa8ead0407be81704af31409f16bd7c4fedadd6
Eddaoust <edmond.daoust@gmail.com>
2019-06-25 11:50:25 +0200

    Update jQuery version & enqueue scripts

--- wp-content/themes/eddst-portfolio/README.md
+++ wp-content/themes/eddst-portfolio/README.md
@@ -2,7 +2,7 @@
 
 **This is my very first Wordpress Theme**
 
-## Usage
+## Start
 
 * Create a homepage and set it as a static page
 * For adding section, create new pages and set the static page as parent
@@ -10,11 +10,23 @@
     * fullname: your fullname
     * job: your job name
     * nickname: your nickname
+* Creating top navbar:
+    * Create a menu named 'navbar'
+    * Add personnal link:
+        * url: #linkname
+        * text: linkname
+    * Select the location 'Navbar'
     
 ### About section
 
 * Add small description of you (text)
 * Add one thumbnail
+* Creating the link button menu:
+    * Create a menu named 'weblink'
+    * Add personnal link:
+        * url: your url link
+        * text: the html code from fontawesome like this ```<i class="fab fa-twitter"></i>```
+        * Select the location 'Weblink'
 
 ## Infos
 
--- wp-content/themes/eddst-portfolio/functions.php
+++ wp-content/themes/eddst-portfolio/functions.php
@@ -7,8 +7,18 @@ function load_stylesheets() {
     wp_register_style('style', get_template_directory_uri().'/style.css');
     wp_enqueue_style('style');
 }
-
 add_action('wp_enqueue_scripts', 'load_stylesheets');
+
+function load_js() {
+    wp_deregister_script('jquery-core');
+    wp_register_script('jquery-core', get_template_directory_uri().'/assets/js/jquery.js', false, '3.4.1', true);
+    wp_deregister_script('jquery-migrate');
+    wp_register_script('jquery-migrate', get_template_directory_uri().'/assets/js/jquery-migrate.js', false, '3.4.1', true);
+    wp_enqueue_script('jquery-core', get_template_directory_uri().'/assets/js/jquery.js', false, '3.4.1', true);
+    wp_enqueue_script('jquery-migrate', get_template_directory_uri().'/assets/js/jquery-migrate.js', false, '3.4.1', true);
+    wp_enqueue_script('skills', get_template_directory_uri().'/assets/js/skills.js', false, '1.0', true);
+}
+add_action('wp_enqueue_scripts', 'load_js');
 
 // Add Menu control on apparence
 add_theme_support('menus');
